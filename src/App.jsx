eb39831860ac6c0bf7e9872119b29fd235201188
axldev <axldev96@gmail.com>
2022-02-25 22:07:48 -0400

    ok

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-4xl">Hello Vite</h1>
+    </>
+  );
+}
+
+export default App;
